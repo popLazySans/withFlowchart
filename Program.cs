fd4fff09d1d04c76c4d911c1ac3e5d1640ea37f6
IamPimdedd <hasamu.44@gmail.com>
2021-06-04 23:25:40 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace fromFlowChart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Sleeptime;
+            do
+            {
+                Console.WriteLine("When are you go to bed?");
+                Sleeptime = double.Parse(Console.ReadLine());
+            } while (Sleeptime < 0 || Sleeptime > 24);
+            double AlarmTime = Sleeptime + 8;
+            if(AlarmTime>=6 && AlarmTime <= 12)
+            {
+
+            }
+            else if (AlarmTime>12 && AlarmTime<=18)
+            {
+                AlarmTime = 12.00;
+            }
+            else
+            {
+                AlarmTime = 6.00;
+            }
+
+            bool Awake;
+            do
+            {
+                Console.WriteLine("{0:.00}",AlarmTime);
+                Console.WriteLine("Are you wake up?(True or False)");
+                Awake = bool.Parse(Console.ReadLine());
+                if (Awake == true)
+                {
+                   
+                }
+                else
+                {
+                    AlarmTime = AlarmTime + 1;
+                }
+            } while (Awake == false);
+        }
+    }
+}
